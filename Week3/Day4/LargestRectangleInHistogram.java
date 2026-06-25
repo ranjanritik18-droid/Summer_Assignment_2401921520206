7f4fa3643ed3c561605656ce04ce8e6742b5f48e
Ritik Ranjan Mishra <ranjanritik18@gmail.com>
2026-06-25 17:14:44 +0530

    Implement largestRectangleArea method in Solution class

---
+++ Week3/Day4/LargestRectangleInHistogram.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int maxArea = 0;
+        int n = heights.length;
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                right[stack.pop()] = i;
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1;
+            int area = heights[i] * width;
+            maxArea = Math.max(maxArea, area);
+        }
+      
+        return maxArea;
+        
+    }
+}
